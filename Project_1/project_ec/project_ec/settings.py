4a49f069b52ff619f1a26116d85ea27ff41e7191
karthi7257552 <karthikkarthi7257552.com>
2025-02-19 16:30:03 +0530

    aa

--- Project_1/project_ec/project_ec/settings.py
+++ Project_1/project_ec/project_ec/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'project_ec',
     'Customers',
     'Products',
     'Orders',
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'project_ec.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,12 +121,20 @@ USE_I18N = True
 USE_TZ = True
 
 
+import os
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS=[
+    BASE_DIR/ 'static',  # project static files
+]
+MEDIA_URL='media/'
+MEDIA_ROOT= BASE_DIR / 'media' 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
